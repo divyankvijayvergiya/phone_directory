0fc7dd1b37988f2def44207e2d0b86521759912b
DIVYANG VIJAYVVARGIYA <divyangvijayvvargiya@192.168.0.107>
2020-12-26 17:37:54 +0530

    data rendered successfully

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './Header';
+import "./App.css";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    let subscribers = [
+      {
+        id: 1,
+        name: "Shilpa",
+        phone:"0000000000"
+      },
+      {
+        id: 2,
+        name: "Srishti",
+        phone:"0000000000"
+      }
+  ];
+    return (
+      <div>
+
+        <Header />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+          {
+            subscribers.map(sub=>{
+              return  <div key={sub.id} className="grid-container">
+              <span className="grid-item">{sub.name}</span>
+              <span className="grid-item">{sub.phone}</span>
+            </div>
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
